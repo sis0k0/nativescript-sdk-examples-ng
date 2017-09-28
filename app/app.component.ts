c5fa47f50226b3105342597e06aeebc668e36015
vakrilov <alexander.vakrilov@telerik.com>
2017-09-28 09:51:54 +0200

    feat: Nav logging

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { Router, NavigationError } from "@angular/router";
+import "rxjs";
 
 @Component({
     moduleId: module.id,
     selector: "sdk-app",
     template: `<page-router-outlet></page-router-outlet>`
 })
-
 export class AppComponent {
+    constructor(router: Router) {
+        router.events
+            .filter(e => e instanceof NavigationError)
+            .subscribe(error => console.log(error.toString()));
+    }
 }
